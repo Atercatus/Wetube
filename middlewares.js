e73f5bc09b23e727f528530994120fb946b29999
AterCatus <myleelee2000@nate.com>
2019-04-09 02:18:50 +0900

    Implement Form, Login and joinus scss code

--- middlewares.js
+++ middlewares.js
@@ -1,20 +1,20 @@
 import routes from "./routes";
-import multer from "multer"
+import multer from "multer";
 
-const multerVideo = multer({dest: "uploads/videos/"});
+const multerVideo = multer({ dest: "uploads/videos/" });
 
 // export를 써야 다른 곳에서 사용 가능
 // local var to global var
 export const localsMiddleware = (req, res, next) => {
-    res.locals.siteName = 'WeTube';
-    res.locals.routes = routes;
-    // req.locals는 res.locals에 이미 변수가 있을 겉 같을때 사용한다
-    // 권장사항은 아니다.
-    res.locals.user = {
-        isAuthenticated: true,
-        id: 1
-    };
-    next();
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  // req.locals는 res.locals에 이미 변수가 있을 겉 같을때 사용한다
+  // 권장사항은 아니다.
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 1
+  };
+  next();
 };
 
-export const uploadVideo = multerVideo.single('videoFile');
+export const uploadVideo = multerVideo.single("videoFile");
